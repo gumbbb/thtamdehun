e328a3e842b67b47468152e042a2aace7ce2136e
gumbbb <87246397+gumbbb@users.noreply.github.com>
2021-11-08 21:53:01 +0700

    Add files via upload

---
+++ thtamdehun-main/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Àn nhong xê yooo!',
+    introDesc: `Trái đất có 7 tỉ ngừi
+    đố iem biết ai dẻ thưn nhấc`,
+    btnIntro: 'Click to find out',
+    title: 'Dẻ hưn nhấc thế giớiii :>>>',
+    desc: 'Đố biếc luông :3333 ',
+    btnYes: 'hong phải TNTA thì làm chóa',
+    btnNo: 'thtam.24 chứ ai',
+    question:'Hỏi lại lần cúi nè ai dẻ hưn nhấc',
+    btnReply: 'Ahihiihii ấn dô để tìm đáp án cuối cùng neee',
+    reply: 'chắc chắn là TNTA ùi :>',
+    mess: 'đùa thui phại là Nguyễn Thanh Tâm chớ bất ngờ chưa sập pờ rai chuaaa 😘😘',
+    messDesc: 'hợp lí chuaaa.',
+    btnAccept: 'IB với ngừi kewt nhấc dũ trụ nefooo :3 ',
+    messLink: 'https://m.me/as.cacf' //m.melink mess của các bạn. VD: https://m.me/nam.nodemy
+}
